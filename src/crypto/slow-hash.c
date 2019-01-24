2844de8b50c3da8840437df0392b030a6f6e556b
Lithy <thelitheproject@gmail.com>
2019-01-24 16:17:43 +0000

    Force heap (ARM)
    Expose preprocessor definition FORCE_USE_HEAP

--- src/crypto/slow-hash.c
+++ src/crypto/slow-hash.c
@@ -2,6 +2,7 @@
 // Copyright (c) 2014-2018, The Monero Project
 // Copyright (c) 2014-2018, The Aeon Project
 // Copyright (c) 2018, The TurtleCoin Developers
+// Copyright (c) 2019, The Lithe Project
 //
 // Please see the included LICENSE file for more information.
 
@@ -1047,6 +1048,7 @@ void cn_slow_hash(const void *data, size_t length, char *hash, int light, int va
 #ifndef FORCE_USE_HEAP
   RDATA_ALIGN16 uint8_t hp_state[page_size];
 #else
+#warning "ACTIVATING FORCE_USE_HEAP IN aarch64 + crypto in slow-hash.c"
   uint8_t *hp_state = (uint8_t *)aligned_malloc(page_size,16);
 #endif
 
@@ -1283,6 +1285,7 @@ void cn_slow_hash(const void *data, size_t length, char *hash, int light, int va
 #ifndef FORCE_USE_HEAP
   uint8_t long_state[page_size];
 #else
+#warning "ACTIVATING FORCE_USE_HEAP IN aarch64 + crypto in slow-hash.c"
   uint8_t *long_state = (uint8_t *)malloc(page_size);
 #endif
 
@@ -1469,6 +1472,7 @@ void cn_slow_hash(const void *data, size_t length, char *hash, int light, int va
 #ifndef FORCE_USE_HEAP
   uint8_t long_state[page_size];
 #else
+#warning "ACTIVATING FORCE_USE_HEAP IN aarch64 + crypto in slow-hash.c"
   uint8_t *long_state = (uint8_t *)malloc(page_size);
 #endif
 
